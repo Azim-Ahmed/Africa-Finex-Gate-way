87bac9674ee165d9e50866f801008856e3d74001
Azim-Ahmed <azimuahmed13@gmail.com>
2021-01-30 17:29:23 +0600

    updated banner to the home page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -10,9 +10,9 @@ const Banner = () => {
                 <Col className="sm-offset-1  my-4" md={{ size: 5, offset: 1 }} sm={12} xs={12}>
                     <Link to="/buySection">
                         <Card body inverse className="Card_background">
-                            <CardTitle tag="h5">Stablecoin gateway - BUY</CardTitle>
-                            <CardText>
-                                With supporting text below as a natural lead-in to additional content.
+                            <CardTitle className="my-4" tag="h3">Stablecoin gateway - BUY</CardTitle>
+                            <CardText className="my-4">
+                                Want to buy stablecoin by your local coin ? <br /> Here you go .
                         </CardText>
                             <Button color="dark" outline={true}><strong>Buy Section</strong></Button>
                         </Card>
@@ -21,9 +21,10 @@ const Banner = () => {
                 <Col className="sm-offset-1  my-4" md={{ size: 5 }} sm={12} xs={12}>
                     <Link to="/stablecoinsellone">
                         <Card body inverse className="Card_background">
-                            <CardTitle tag="h5">Stablecoin gateway - SELL</CardTitle>
-                            <CardText>
-                                With supporting text below as a natural lead-in to additional content.
+                            <CardTitle className="my-4" tag="h3">Stablecoin gateway - SELL</CardTitle>
+                            <CardText className="my-4">
+                                Want to sell your stablecoin with trust ?
+                                <br /> you are in the right place.
                         </CardText>
                             <Button color="dark" outline={true}> <strong> Sell Section</strong></Button>
                         </Card>
